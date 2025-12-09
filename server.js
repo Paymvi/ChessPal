5648820457d16f194bfd90f9473202aa59eaa363
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2025-12-09 13:34:17 -0500

    Fixed server.js

--- server.js
+++ server.js
@@ -291,9 +291,10 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "client/dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
+
 
 //------------------------------------------------------------
 // LISTEN (MUST BE LAST)
